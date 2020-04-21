8c86d32f294d14779d059964aabd6f4abe50402a
jli09 <56615820+jli09@users.noreply.github.com>
2020-04-21 15:20:34 -0400

    updated User model test in db/models/user.spec

--- server/db/models/user.spec.js
+++ server/db/models/user.spec.js
@@ -11,21 +11,37 @@ describe('User model', () => {
 
   describe('instanceMethods', () => {
     describe('correctPassword', () => {
-      let cody
+      let cody, admin
 
       beforeEach(async () => {
         cody = await User.create({
           email: 'cody@puppybook.com',
           password: 'bones'
         })
+
+        admin = await User.create({
+          email: 'admin@admin.com',
+          password: 'the boss',
+          isAdmin: true
+        })
       })
 
       it('returns true if the password is correct', () => {
         expect(cody.correctPassword('bones')).to.be.equal(true)
+        expect(admin.correctPassword('the boss')).to.be.equal(true)
       })
 
       it('returns false if the password is incorrect', () => {
         expect(cody.correctPassword('bonez')).to.be.equal(false)
+        expect(admin.correctPassword('not boss')).to.be.equal(false)
+      })
+
+      it('sets isAdmin to false by default', () => {
+        expect(cody.isAdmin).to.be.equal(false)
+      })
+
+      it('sets isAdmin to true for admins', () => {
+        expect(admin.isAdmin).to.be.equal(true)
       })
     }) // end describe('correctPassword')
   }) // end describe('instanceMethods')
