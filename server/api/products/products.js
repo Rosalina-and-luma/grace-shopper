6d5af340403384479b44ed8354fa8258059e1987
Mohana Bansal <mohanabansal10@gmail.com>
2020-04-21 14:08:27 -0400

    created BE route for /api/products/:productId

--- server/api/products/products.js
+++ server/api/products/products.js
@@ -2,4 +2,20 @@ const router = require('express').Router()
 
 // import products model
 
+router.get('/:productId', async (req, res, next) => {
+  try {
+    // const selectedProduct = await Product.findByPk(req.params.productId);
+    // res.json(selectedProduct)
+    const temp = {
+      id: 1,
+      name: 'The Elder Wand',
+      price: '$45.00',
+      description: 'This is the most powerful wand in the history!'
+    }
+    res.json(temp)
+  } catch (error) {
+    next(error)
+  }
+})
+
 module.exports = router
