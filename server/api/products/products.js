8da541b688cc2bff6cd3bfffdc884654a893cf54
Nora Lashner <nlashner@gmail.com>
2020-04-21 13:09:06 -0400

    created all product get route in /server/api/products and added require product line in /api/index

--- server/api/products/products.js
+++ server/api/products/products.js
@@ -2,4 +2,14 @@ const router = require('express').Router()
 
 // import products model
 
+
+router.get('/', async (req, res, next) => {
+  try {
+    const products = await Products.findAll()
+    res.json(products)
+  } catch(err) {
+    next(err)
+  }
+})
+
 module.exports = router
