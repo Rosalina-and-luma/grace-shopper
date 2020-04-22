3be5f89ef044e1b71f123d70985d6e20b8071ce7
Mohana Bansal <mohanabansal10@gmail.com>
2020-04-22 15:48:44 -0400

    cleaned up code

--- client/components/singleProduct/index.js
+++ client/components/singleProduct/index.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import {NavLink} from 'react-router-dom'
 import {fetchSingleProduct} from '../../reducer/singleProduct'
 
 class SingleProduct extends Component {
