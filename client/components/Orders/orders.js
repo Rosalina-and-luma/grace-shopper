9c5201b39e092cb979b9a4c9f6283efaa67759a4
Mohana Bansal <mohanabansal10@gmail.com>
2020-04-23 22:08:57 -0400

    edit cart completed

--- client/components/Orders/orders.js
+++ client/components/Orders/orders.js
@@ -1,39 +1,118 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import {getOrdersFromServer} from '../../reducer/order/order'
+import {getOrdersFromServer, addToCartServer} from '../../reducer/order/order'
 import './orders.css'
 
 class Orders extends Component {
-  componentDidMount() {
-    this.props.getOrders(this.props.user.id)
+  constructor() {
+    super()
+    this.state = {
+      allProducts: []
+    }
   }
+  componentDidMount = async () => {
+    await this.props.getOrders(this.props.user.id)
+    let products = []
+    this.props.orders.map(order => {
+      if (!order.purchased) {
+        for (let i = 0; i < order.products.length; i++) {
+          let prod = order.products[i]
+          let orderProd = order.order_products[i]
+          products.push({
+            id: prod.id,
+            imgUrl: prod.imgUrl,
+            name: prod.name,
+            unitPrice: orderProd.unitPrice,
+            quantity: orderProd.quantity
+          })
+        }
+      }
+    })
+
+    this.setState({
+      allProducts: products.map(prod => ({
+        id: prod.id,
+        imgUrl: prod.imgUrl,
+        name: prod.name,
+        unitPrice: prod.unitPrice,
+        quantity: prod.quantity
+      }))
+    })
+  }
+
+  addQuantity = id => {
+    console.log('id', id)
+    let updatedQuantity
+    let oldProds = [...this.state.allProducts]
+    let newProds = oldProds.map(prod => {
+      if (prod.id === id) {
+        prod.quantity += 1
+        updatedQuantity = prod.quantity
+      }
+      return prod
+    })
+    this.setState({
+      allProducts: [...newProds]
+    })
+    this.props.updateOrders({
+      userId: this.props.user.id,
+      productId: id,
+      quantity: updatedQuantity
+    })
+  }
+
+  subtractQuantity = id => {
+    let updatedQuantity
+    let oldProds = [...this.state.allProducts]
+    let newProds = oldProds.map(prod => {
+      if (prod.id === id) {
+        prod.quantity -= 1
+        updatedQuantity = prod.quantity
+      }
+      return prod
+    })
+    this.setState({
+      allProducts: [...newProds]
+    })
+    this.props.updateOrders({
+      userId: this.props.user.id,
+      productId: id,
+      quantity: updatedQuantity
+    })
+  }
+
   render() {
     return (
       <div>
         <h1>Here are your orders</h1>
-        {this.props.orders.map(order => {
-          let products = []
-          if (!order.purchased) {
-            for (let i = 0; i < order.products.length; i++) {
-              let prod = order.products[i]
-              let orderProd = order.order_products[i]
-              products.push({
-                key: prod.id,
-                src: prod.imgUrl,
-                name: prod.name,
-                unitPrice: orderProd.unitPrice,
-                quantity: orderProd.quantity
-              })
-            }
-          }
-          return products.map(product => (
+        {this.state.allProducts.map(product => {
+          return (
             <div key={product.id} className="orders-section">
-              <img src={product.src} />
+              <img src={product.imgUrl} />
               <span className="name">{product.name}</span>
               <span className="unitPrice">{product.unitPrice}</span>
               <span className="quantity">{product.quantity}</span>
+              <button
+                type="button"
+                onClick={() => {
+                  this.addQuantity(product.id)
+                }}
+              >
+                +
+              </button>
+              <label className="quantity" name="quantity">
+                {product.quantity}
+              </label>
+              <button
+                type="button"
+                onClick={() => {
+                  this.subtractQuantity(product.id)
+                }}
+              >
+                -
+              </button>
             </div>
-          ))
+          )
         })}
       </div>
     )
@@ -49,7 +128,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    getOrders: userId => dispatch(getOrdersFromServer(userId))
+    getOrders: userId => dispatch(getOrdersFromServer(userId)),
+    updateOrders: order => dispatch(addToCartServer(order))
   }
 }
 
