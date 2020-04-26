03096521f291890dcf86a029830a78f8456ec798
jli09 <56615820+jli09@users.noreply.github.com>
2020-04-26 18:27:09 -0400

    refactored seed file

--- script/seed.js
+++ script/seed.js
@@ -47,7 +47,7 @@ async function seed() {
       imgUrl:
         'https://vignette.wikia.nocookie.net/harrypotter/images/5/59/Elder_Wand.png/revision/latest?cb=20161128051519',
       description: '35.5cm, the finest elderwood',
-      price: 500,
+      price: 500 * 100,
       categoryId: 1,
       inventory: 50
     }),
@@ -56,7 +56,7 @@ async function seed() {
       imgUrl:
         'https://static.wixstatic.com/media/5ec770_d8536997be814c1a885a5c27eeece84a~mv2.jpg/v1/fill/w_485,h_485,al_c,lg_1,q_85/5ec770_d8536997be814c1a885a5c27eeece84a~mv2.jpg',
       description: '34.5cm, guaranteed by Fred and George for all your pranks',
-      price: 125,
+      price: 125 * 100,
       categoryId: 1,
       inventory: 700
     }),
@@ -65,7 +65,7 @@ async function seed() {
       imgUrl:
         'https://ottosgranary.com/wp-content/uploads/2019/04/mtmzk6ry5z3d4wnyyukh.jpg',
       description: 'nimble and fast, great for seekers',
-      price: 7800,
+      price: 7800 * 100,
       categoryId: 2,
       inventory: 20
     }),
@@ -73,7 +73,7 @@ async function seed() {
       name: 'Firebolt',
       imgUrl: 'https://i.ebayimg.com/images/g/kfsAAOSwLSZcNTkL/s-l640.jpg',
       description: 'cutting edge flying technology',
-      price: 16000,
+      price: 16000 * 100,
       categoryId: 2,
       inventory: 10
     }),
@@ -82,7 +82,7 @@ async function seed() {
       imgUrl:
         'https://cdn.shopify.com/s/files/1/1541/8579/products/Robe-Adults-Gryffindor-HarryPotter-Product-_6_grande.jpg?v=1586234451',
       description: 'for brave and loyal souls (wand not included)',
-      price: 200,
+      price: 200 * 100,
       categoryId: 3,
       inventory: 120
     }),
@@ -91,7 +91,7 @@ async function seed() {
       imgUrl:
         'https://cdn.shopify.com/s/files/1/1541/8579/products/Slytherin_Robe_5_grande.jpg?v=1586234778',
       description: 'for those with cunning ambition (wand not included)',
-      price: 200,
+      price: 200 * 100,
       categoryId: 3,
       inventory: 120
     }),
@@ -100,7 +100,7 @@ async function seed() {
       imgUrl:
         'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRlGeKx8KV6jE2Hc3dVZBI06_z8DheNuE-t4sa5Yhlh2_nOdU6FpskT8TEEsGc8eOIhj-UD7J8&usqp=CAc',
       description: `reveal your heart's desires`,
-      price: 40000,
+      price: 40000 * 100,
       categoryId: 4,
       inventory: 2
     }),
@@ -109,7 +109,7 @@ async function seed() {
       imgUrl:
         'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQz6Gwh6yWjI-5_-bw5TRoVg8BVlnblfUOGsRte6_GgGkFTPOAR1PZVeb4mBlUxD5gRmQcGgnhE&usqp=CAc',
       description: 'revisit your fondest memories, literally',
-      price: 80000,
+      price: 80000 * 100,
       categoryId: 4,
       inventory: 1
     })
@@ -130,14 +130,14 @@ async function seed() {
     productId: 4,
     orderId: codyOrder.id,
     quantity: 2,
-    unitPrice: 16000
+    unitPrice: 16000 * 100
   })
 
   await OrderProduct.create({
     orderId: codyOrder.id,
     productId: 6,
     quantity: 4,
-    unitPrice: 200
+    unitPrice: 200 * 100
   })
 
   const murphy = await User.findOne({
@@ -155,7 +155,7 @@ async function seed() {
     orderId: murphyOrder.id,
     productId: 5,
     quantity: 4,
-    unitPrice: 200
+    unitPrice: 200 * 100
   })
 
   await murphyOrder.update({purchased: true})
