141f4f8d476b43fff72734ab13735355fd13c7eb
Nora Lashner <nlashner@gmail.com>
2020-04-26 21:33:59 -0400

    updated signup form on signup page

--- client/components/Signup/signup.js
+++ client/components/Signup/signup.js
@@ -23,78 +23,84 @@ class Signup extends Component {
 
   render() {
     return (
-      <div className="signup-form">
-        <div className="name">
+      <div>
+        <div className="title">
+          <h1>Create a Diagon E-lley Account</h1>
+        </div>
+        <div className="signup-form">
+          <div className="name-container">
+            <label>
+              First Name
+              {!this.state.firstName.trim() && (
+                <span className="warning">(required)</span>
+              )}
+            </label>
+            <input
+              type="text"
+              name="firstName"
+              value={this.state.firstName}
+              onChange={this.handleChange}
+            />
+            <br />
+
+            <label>
+              Last Name
+              {!this.state.lastName.trim() && (
+                <span className="warning">(required)</span>
+              )}
+            </label>
+            <input
+              type="text"
+              name="lastName"
+              value={this.state.lastName}
+              onChange={this.handleChange}
+            />
+            <br />
+          </div>
+
           <label>
-            First Name
-            {!this.state.firstName.trim() && (
+            Email
+            {!this.state.email.trim() && (
               <span className="warning">(required)</span>
             )}
           </label>
           <input
             type="text"
-            name="firstName"
-            value={this.state.firstName}
+            name="email"
+            value={this.state.email}
             onChange={this.handleChange}
           />
           <br />
-
           <label>
-            Last Name
-            {!this.state.lastName.trim() && (
-              <p className="warning">(required)</p>
+            Password
+            {!this.state.password.trim() && (
+              <span className="warning">(required)</span>
             )}
           </label>
           <input
-            type="text"
-            name="lastName"
-            value={this.state.lastName}
+            type="password"
+            name="password"
+            value={this.state.password}
             onChange={this.handleChange}
           />
           <br />
+          <NavLink to="/products">
+            <button
+              type="button"
+              onClick={() => {
+                this.props.addUser({
+                  firstName: this.state.firstName,
+                  lastName: this.state.lastName,
+                  email: this.state.email,
+                  password: this.state.password
+                })
+              }}
+            >
+              {' '}
+              Signup
+            </button>
+          </NavLink>
         </div>
-        <label>
-          Email
-          {!this.state.email.trim() && (
-            <span className="warning">(required)</span>
-          )}
-        </label>
-        <input
-          type="text"
-          name="email"
-          value={this.state.email}
-          onChange={this.handleChange}
-        />
-        <br />
-        <label>
-          Password
-          {!this.state.password.trim() && (
-            <span className="warning">(required)</span>
-          )}
-        </label>
-        <input
-          type="password"
-          name="password"
-          value={this.state.password}
-          onChange={this.handleChange}
-        />
-        <br />
-        <NavLink to="/products">
-          <button
-            type="button"
-            onClick={() => {
-              this.props.addUser({
-                firstName: this.state.firstName,
-                lastName: this.state.lastName,
-                email: this.state.email,
-                password: this.state.password
-              })
-            }}
-          >
-            {' '}
-            Signup
-          </button>
-        </NavLink>
       </div>
     )
   }
