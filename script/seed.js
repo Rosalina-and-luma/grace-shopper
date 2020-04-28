203e14232f01c550d56ec39cd884dc9e3d775642
Arianna Campesi <Ariannafrancampesi@gmail.com>
2020-04-28 13:30:03 -0400

    fixed prices on seed file and added setter to price on product model, made changed in script/seed.js and server/db/models/product.js

--- script/seed.js
+++ script/seed.js
@@ -140,7 +140,7 @@ async function seed() {
       imgUrl:
         'https://vignette.wikia.nocookie.net/harrypotter/images/5/59/Elder_Wand.png/revision/latest?cb=20161128051519',
       description: '35.5cm, the finest elderwood',
-      price: 500 * 100,
+      price: 500,
       categoryId: 1,
       inventory: 52
     }),
@@ -149,7 +149,7 @@ async function seed() {
       imgUrl:
         'https://static.wixstatic.com/media/5ec770_d8536997be814c1a885a5c27eeece84a~mv2.jpg/v1/fill/w_485,h_485,al_c,lg_1,q_85/5ec770_d8536997be814c1a885a5c27eeece84a~mv2.jpg',
       description: '34.5cm, guaranteed by Fred and George for all your pranks',
-      price: 125 * 100,
+      price: 125,
       categoryId: 1,
       inventory: 139
     }),
@@ -158,7 +158,7 @@ async function seed() {
       imgUrl:
         'https://vignette.wikia.nocookie.net/hpwands/images/d/db/GWwand.jpg/revision/latest/scale-to-width-down/340?cb=20141103054542',
       description: '35cm',
-      price: 225 * 100,
+      price: 225,
       categoryId: 1,
       inventory: 122
     }),
@@ -167,7 +167,7 @@ async function seed() {
       imgUrl:
         'https://cdn.shopify.com/s/files/1/2597/5112/products/hpnbwand_1_1024x1024@2x.jpg?v=1575933194',
       description: '33.5cm',
-      price: 525 * 100,
+      price: 525,
       categoryId: 1,
       inventory: 42
     }),
@@ -176,7 +176,7 @@ async function seed() {
       imgUrl:
         'https://www.renderhub.com/dmitriykotliar/wand-of-luna-lovegood-from-the-movie-harry-potter/wand-of-luna-lovegood-from-the-movie-harry-potter-04.jpg',
       description: '34.5cm',
-      price: 225 * 100,
+      price: 225,
       categoryId: 1,
       inventory: 184
     }),
@@ -185,7 +185,7 @@ async function seed() {
       imgUrl:
         'https://vignette.wikia.nocookie.net/harrypotter/images/c/c6/Percival_Graves_wand.png/revision/latest?cb=20161128064141',
       description: '36.5cm',
-      price: 125 * 100,
+      price: 125,
       categoryId: 1,
       inventory: 103
     }),
@@ -194,7 +194,7 @@ async function seed() {
       imgUrl:
         'https://cdn.giftsdigest.com/wp-content/uploads/2018/12/ron-weasley-s-wand.jpg',
       description: '34.5cm',
-      price: 155 * 100,
+      price: 155,
       categoryId: 1,
       inventory: 86
     }),
@@ -203,7 +203,7 @@ async function seed() {
       imgUrl:
         'https://images-na.ssl-images-amazon.com/images/I/61uMOXYj4RL._AC_SL1500_.jpg',
       description: '38cm',
-      price: 175 * 100,
+      price: 175,
       categoryId: 1,
       inventory: 77
     }),
@@ -212,7 +212,7 @@ async function seed() {
       imgUrl:
         'https://cdn11.bigcommerce.com/s-z5x6hb/images/stencil/1280x1280/products/13923/16834/b25d4895-b184-4a50-94bb-7ec66bdf0f89__56515.1529076234.jpg?c=2?imbypass=on',
       description: '37cm',
-      price: 145 * 100,
+      price: 145,
       categoryId: 1,
       inventory: 28
     }),
@@ -221,7 +221,7 @@ async function seed() {
       imgUrl:
         'https://cdn.shopify.com/s/files/1/2597/5112/products/hpnbwandgin_300x300.jpg?v=1575933194',
       description: '36.5cm',
-      price: 145 * 100,
+      price: 145,
       categoryId: 1,
       inventory: 27
     }),
@@ -230,7 +230,7 @@ async function seed() {
       imgUrl:
         'https://vignette.wikia.nocookie.net/hpwands/images/1/17/MMwand.jpg/revision/latest/scale-to-width-down/340?cb=20141103234937',
       description: '35cm',
-      price: 75 * 100,
+      price: 75,
       categoryId: 1,
       inventory: 34
     }),
@@ -239,7 +239,7 @@ async function seed() {
       imgUrl:
         'https://cdn.shopify.com/s/files/1/2597/5112/products/hpnbwandnl02_1_1024x1024@2x.jpg?v=1575933195',
       description: '34cm',
-      price: 75 * 100,
+      price: 75,
       categoryId: 1,
       inventory: 29
     }),
@@ -248,7 +248,7 @@ async function seed() {
       imgUrl:
         'https://images-na.ssl-images-amazon.com/images/I/41BILH8YS4L._AC_SL1080_.jpg',
       description: '36.5cm',
-      price: 175 * 100,
+      price: 175,
       categoryId: 1,
       inventory: 52
     }),
@@ -257,7 +257,7 @@ async function seed() {
       imgUrl:
         'https://vignette.wikia.nocookie.net/harrypotter/images/3/30/Severus_Snape_wand.png/revision/latest?cb=20161128052653',
       description: '38cm',
-      price: 205 * 100,
+      price: 205,
       categoryId: 1,
       inventory: 12
     }),
@@ -266,7 +266,7 @@ async function seed() {
       imgUrl:
         'https://vignette.wikia.nocookie.net/harrypotter/images/9/9e/Sirius_Black_wand.png/revision/latest?cb=20161128052654',
       description: '40cm',
-      price: 75 * 100,
+      price: 75,
       categoryId: 1,
       inventory: 28
     }),
@@ -275,7 +275,7 @@ async function seed() {
       imgUrl:
         'https://ottosgranary.com/wp-content/uploads/2019/04/mtmzk6ry5z3d4wnyyukh.jpg',
       description: 'nimble and fast, great for seekers',
-      price: 7800 * 100,
+      price: 7800,
       categoryId: 2,
       inventory: 20
     }),
@@ -283,7 +283,7 @@ async function seed() {
       name: 'Firebolt',
       imgUrl: 'https://i.ebayimg.com/images/g/kfsAAOSwLSZcNTkL/s-l640.jpg',
       description: 'cutting edge flying technology',
-      price: 16000 * 100,
+      price: 16000,
       categoryId: 2,
       inventory: 10
     }),
@@ -292,7 +292,7 @@ async function seed() {
       imgUrl:
         'https://images-na.ssl-images-amazon.com/images/I/31PgXGkd04L._AC_.jpg',
       description: 'fly like Mr. Potter himself',
-      price: 15000 * 100,
+      price: 15000,
       categoryId: 2,
       inventory: 5
     }),
@@ -301,7 +301,7 @@ async function seed() {
       imgUrl:
         'https://cdn.shopify.com/s/files/1/1541/8579/products/Robe-Adults-Gryffindor-HarryPotter-Product-_6_grande.jpg?v=1586234451',
       description: 'for brave and loyal souls (wand not included)',
-      price: 200 * 100,
+      price: 200,
       categoryId: 3,
       inventory: 120
     }),
@@ -310,7 +310,7 @@ async function seed() {
       imgUrl:
         'https://cdn.shopify.com/s/files/1/1541/8579/products/Slytherin_Robe_5_grande.jpg?v=1586234778',
       description: 'for those with cunning ambition (wand not included)',
-      price: 200 * 100,
+      price: 200,
       categoryId: 3,
       inventory: 120
     }),
@@ -319,7 +319,7 @@ async function seed() {
       imgUrl:
         'https://cdn.shopify.com/s/files/1/1541/8579/products/Robe-Kids-Hufflepuff-HarryPotter-Product-_5_118f1f7e-0201-40de-939a-f702d64a751a.jpg?v=1543301078',
       description: 'The official robe of the Hufflepuff house',
-      price: 200 * 100,
+      price: 200,
       categoryId: 3,
       inventory: 150
     }),
@@ -328,7 +328,7 @@ async function seed() {
       imgUrl:
         ' https://cdn.shopify.com/s/files/1/1541/8579/products/Ravenclaw_Robe_5_grande.jpg?v=1586234574',
       description: 'for those who are witty and creative',
-      price: 200 * 100,
+      price: 200,
       categoryId: 3,
       inventory: 140
     }),
@@ -337,7 +337,7 @@ async function seed() {
       imgUrl:
         'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRlGeKx8KV6jE2Hc3dVZBI06_z8DheNuE-t4sa5Yhlh2_nOdU6FpskT8TEEsGc8eOIhj-UD7J8&usqp=CAc',
       description: `reveal your heart's desires`,
-      price: 40000 * 100,
+      price: 40000,
       categoryId: 4,
       inventory: 2
     }),
@@ -346,7 +346,7 @@ async function seed() {
       imgUrl:
         'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQz6Gwh6yWjI-5_-bw5TRoVg8BVlnblfUOGsRte6_GgGkFTPOAR1PZVeb4mBlUxD5gRmQcGgnhE&usqp=CAc',
       description: 'revisit your fondest memories, literally',
-      price: 80000 * 100,
+      price: 80000,
       categoryId: 4,
       inventory: 1
     }),
@@ -355,7 +355,7 @@ async function seed() {
       imgUrl:
         'https://gamepress.gg/wizardsunite/wizardsunite/sites/wizardsunite/files/2019-05/Veritaserum-foundable.png',
       description: 'hear the truth with this potion',
-      price: 300 * 100,
+      price: 300,
       categoryId: 4,
       inventory: 1
     })
@@ -409,67 +409,67 @@ async function seed() {
       orderId: codyOrder.id,
       productId: 4,
       quantity: 2,
-      unitPrice: 16000 * 100
+      unitPrice: 16000
     },
     {
       orderId: codyOrder.id,
       productId: 2,
       quantity: 4,
-      unitPrice: 16000 * 100
+      unitPrice: 16000
     },
     {
       orderId: codyOrder.id,
       productId: 6,
       quantity: 3,
-      unitPrice: 200 * 100
+      unitPrice: 200
     },
     {
       orderId: murphyOrder.id,
       productId: 5,
       quantity: 4,
-      unitPrice: 200 * 100
+      unitPrice: 200
     },
     {
       orderId: murphyOrder.id,
       productId: 1,
       quantity: 4,
-      unitPrice: 500 * 100
+      unitPrice: 500
     },
     {
       orderId: hagridOrder.id,
       productId: 1,
       quantity: 4,
-      unitPrice: 500 * 100
+      unitPrice: 500
     },
     {
       orderId: hagridOrder.id,
       productId: 3,
       quantity: 4,
-      unitPrice: 7800 * 100
+      unitPrice: 7800
     },
     {
       orderId: hagridOrder2.id,
       productId: 7,
       quantity: 8,
-      unitPrice: 40000 * 100
+      unitPrice: 40000
     },
     {
       orderId: hagridOrder2.id,
       productId: 8,
       quantity: 2,
-      unitPrice: 80000 * 100
+      unitPrice: 80000
     },
     {
       orderId: hagridOrder3.id,
       productId: 5,
       quantity: 4,
-      unitPrice: 16000 * 100
+      unitPrice: 16000
     },
     {
       orderId: hagridOrder3.id,
       productId: 2,
       quantity: 2,
-      unitPrice: 500 * 100
+      unitPrice: 500
     }
   ]
 
