e5848098c83bd6ddc017b471d6dd592725c8423a
Nora Lashner <nlashner@gmail.com>
2020-04-28 17:22:37 -0400

    test for user routes passing

--- server/api/users/users.spec.js
+++ server/api/users/users.spec.js
@@ -5,6 +5,7 @@ const request = require('supertest')
 const db = require('../../db')
 const app = require('../../index')
 const User = db.model('user')
+const agent = require('supertest')(app)
 
 describe('User routes', () => {
   beforeEach(() => {
@@ -20,20 +21,21 @@ describe('User routes', () => {
     //   })
     // })
 
+    const user = {
+      firstName: 'harry',
+      lastName: 'potter',
+      email: 'hp@hogwarts.com',
+      password: 'snitch'
+    }
+
     it('unable to acess users api if not an admin', async () => {
-      const res = await request(app)
-        .get('/api/users')
-        .expect(403)
+      const res = await agent.get('/api/users').expect(403)
     })
 
     it('POST method creates a new user in the db', async () => {
-      const res = await request(app)
-        .post('/api/users', {
-          firstName: 'harry',
-          lastName: 'potter',
-          email: 'hp@hogwarts.com',
-          password: 'snitch'
-        })
+      const res = await agent
+        .post('/api/users')
+        .send(user)
         .expect(200)
 
       expect(res.body).to.be.an('object')
