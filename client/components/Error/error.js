4dadcb016a1482ac36e10f30c3f698db7e98d823
Nora Lashner <nlashner@gmail.com>
2020-04-28 20:06:47 -0400

    added error page to routes that don't exist

--- client/components/Error/error.js
+++ client/components/Error/error.js
@@ -1,16 +1,24 @@
-import {React} from 'react'
+import React, {Component} from 'react'
 import {NavLink} from 'react-router-dom'
+import '../Homepage/homepage.css'
 
-const Error = () => {
-  return (
-    <div>
-      <h2>
-        Sorry! Either you do not have access to the required access or kindly
-        check the URL. Click <NavLink to="/"> here </NavLink> to return to the
-        home page
-      </h2>
-    </div>
-  )
+class ErrorPage extends Component {
+  render() {
+    return (
+      <div className="homepage-container">
+        <img
+          src="https://media1.tenor.com/images/3ce94e2c5f30772be78e779750c56112/tenor.gif?itemid=3844286"
+          className="intro-gif"
+        />
+
+        <h2 className="intro">
+          Sorry! Either you do not have access to the required access or kindly
+          check the URL. Click <NavLink to="/"> here </NavLink> to return to the
+          home page
+        </h2>
+      </div>
+    )
+  }
 }
 
-export default Error
+export default ErrorPage
